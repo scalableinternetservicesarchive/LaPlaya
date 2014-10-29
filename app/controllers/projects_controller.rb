23e8086b9f6bdcd1c1478c591bd361d611888c09
Charlotte Hill <charlotte@hillnetwork.com>
2014-10-29 12:13:46 -0700

    create project controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,19 @@
+class ProjectsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def view
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+
+  get 'projects/new'
+
+  get 'projects/edit'
+
+  get 'projects/update'
+
+  get 'projects/view'
+
+  get 'projects/delete'
+
   devise_for :users
   root 'static_pages#home'
 end
