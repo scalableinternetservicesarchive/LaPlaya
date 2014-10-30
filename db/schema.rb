86cacf37afb9115ce5b13facb108745174c34ca1
Charlotte Hill <charlotte@hillnetwork.com>
2014-10-29 18:37:22 -0700

    Undo project controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029044331) do
-
-  create_table "projects", force: true do |t|
-    t.string   "title"
-    t.string   "instructions"
-    t.string   "note"
-    t.string   "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20141025235917) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
