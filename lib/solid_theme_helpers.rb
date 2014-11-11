7284a4047934c37c37efdd03459d4f8510022adf
Johan Henkens <Johan@henkens.com>
2014-11-11 15:17:48 -0800

    Add more helpers

--- lib/solid_theme_helpers.rb
+++ lib/solid_theme_helpers.rb
@@ -8,22 +8,33 @@ module SolidThemeHelpers
     content_tag :div, wrapper_options, &block
   end
 
-  def solid_portfolio_item(text, thumb_url, image_url, link_url, lightbox_title = SecureRandom.hex(10))
-    solid_portfolio_item_wrapper do
+  def solid_mouseover_image(text, thumb_url, &block)
+    content = capture(&block)
+    content_tag :div, class: 'he-wrap tpl6' do
+      (
       concat image_tag thumb_url
       concat (content_tag :div, class: 'he-view' do
                content_tag :div, class: 'bg a0', data: {animate: 'fadeIn'} do
                  concat (content_tag :h3, class: 'a1', data: {animate: 'fadeInDown'} do
                           text
                         end)
-                 concat (link_to image_url, class: 'dmbutton a2', data: {animate: 'fadeInUp', lightbox: lightbox_title} do
-                          '<i class="fa fa-search"></i>'.html_safe
-                        end)
-                 concat (link_to link_url, class: 'dmbutton a2', 'data-animate' => 'fadeInUp' do
-                          '<i class="fa fa-link"></i>'.html_safe
-                        end)
+                 concat content
                end
              end)
+      )
+    end
+  end
+
+  def solid_portfolio_item(text, thumb_url, image_url, link_url, lightbox_title = SecureRandom.hex(10))
+    solid_portfolio_item_wrapper do
+      solid_mouseover_image(text, thumb_url) do
+        concat (link_to image_url, class: 'dmbutton a2', data: {animate: 'fadeInUp', lightbox: lightbox_title} do
+                 '<i class="fa fa-search"></i>'.html_safe
+               end)
+        concat (link_to link_url, class: 'dmbutton a2', 'data-animate' => 'fadeInUp' do
+                 '<i class="fa fa-link"></i>'.html_safe
+               end)
+      end
     end
   end
 
@@ -39,14 +50,14 @@ module SolidThemeHelpers
   end
 
   private
-  def solid_carousel_indicators(id, images,options)
+  def solid_carousel_indicators(id, images, options)
     content_tag :ol, class: 'carousel-indicators' do
       (1..images.length).each_with_index do |v, i|
         __options = {data: {target: "##{id}", 'slide-to' => i.to_s}}
         if options[:active] == i
           __options[:class] = 'active'
         end
-        concat content_tag(:li, __options)
+        concat content_tag(:li, nil, __options)
       end
     end
   end
@@ -65,9 +76,7 @@ module SolidThemeHelpers
   end
 
   def solid_portfolio_item_wrapper(&block)
-    content_tag :div, class: 'portfolio-item graphic-design' do
-      content_tag :div, class: 'he-wrap tpl6', &block
-    end
+    content_tag :div, class: 'portfolio-item graphic-design', &block
   end
 
 end
