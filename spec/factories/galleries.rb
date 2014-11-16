371775b7b862fa9def125de6e19da361091c5409
Kyle Jorgensen <kylewjorgensen@gmail.com>
2014-11-15 23:59:15 -0800

    Galleries now seeded with projects

--- spec/factories/galleries.rb
+++ spec/factories/galleries.rb
@@ -3,8 +3,19 @@
 FactoryGirl.define do
   factory :gallery do
     title { Faker::Lorem.sentence(2, false, 4) }
-    after(:build) do
+    author
 
+    factory :gallery_with_projects do
+
+      # see https://github.com/thoughtbot/factory_girl/blob/v4.4.0/GETTING_STARTED.md#transient-attributes
+      # for what this is doing. I want 5 projects to be created with every gallery
+      ignore do
+        projects_count 5
+      end
+
+      after(:create) do |gallery, evaluator|
+        gallery.projects = FactoryGirl.create_list(:project, evaluator.projects_count)
+      end
     end
   end
 end
