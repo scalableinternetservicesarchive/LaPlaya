0d7b17500c01df4a3e5a615262898ae74801a28a
Johan Henkens <Johan@henkens.com>
2014-11-19 16:54:25 -0800

    Fixed #14

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,6 @@ class Project < ActiveRecord::Base
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :comments, dependent: :destroy
   has_and_belongs_to_many :galleries
-  has_and_belongs_to_many :user_likes, class_name: 'User'
 
   #Project Likes
   has_many :project_likes, dependent: :destroy
