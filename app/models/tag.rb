13c94a34fef807fa30de83f0f069a418d9be0bdd
Charlotte Hill <charlotte@hillnetwork.com>
2014-11-22 11:46:36 -0800

    tag cloud for popular projects

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,9 @@
 class Tag < ActiveRecord::Base
   has_many :taggings
   has_many :projects, through: :taggings
+
+  def self.counts
+    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+  end
+
 end
