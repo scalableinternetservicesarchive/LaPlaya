9ed9f471f098163a1c528c0ff86b7daf63b699b9
EC2 Default User <ec2-user@ip-172-31-3-197.us-west-2.compute.internal>
2014-12-08 03:22:58 +0000

    benchTest.py tested and runs.

--- test-LaPlaya/benchTest.py
+++ test-LaPlaya/benchTest.py
@@ -9,26 +9,23 @@ if len(sys.argv) >= 2:
 
 # test cases, one array entry per test (may be multiple entries per class/file)
 tests = [
-    { "test_CPCreateUser.py", "CPCreateUser", "test_logged_out_signup_path", "cp-createuser-bench.xml" },
-    { "test_CPComments.py", "CPComments", "test_readonly_view_comment", "cp-comments-bench.xml" },
-    { "test_CPShowProjects.py", "CPShowProjects", "test_readonly_view_projects", "cp-showprojects-bench.xml" },
-    { "test_CPHearts.py", "CPHearts", "test_heart_projects", "cp-hearts-bench.xml" }
+    [ "test_CPCreateUser.py", "CPCreateUser", "test_logged_out_signup_path", "cp-createuser-bench.xml" ],
+    [ "test_CPComments.py", "CPComments", "test_readonly_view_comment", "cp-comments-bench.xml" ],
+    [ "test_CPShowProjects.py", "CPShowProjects", "test_readonly_view_projects", "cp-showprojects-bench.xml" ],
+    [ "test_CPHearts.py", "CPHearts", "test_heart_projects", "cp-hearts-bench.xml" ]
     ]
 
 # run tests
 for test in tests:
-    cmdLine = ""
-    if serverURL = None:
-        cmdLine = 'fl-run-bench %s %s.%s' % (test[0], test[1], test[2])
-    else:
-        cmdLine = 'fl-run-bench -u %s %s.%s' % (serverURL, test[0], test[1], test[2])
+    print '  Running %s test' % (test[1])
 
-    subprocess.call([cmdLine])
+    if serverURL == None:
+	subprocess.call( [ 'fl-run-bench %s %s.%s' % ( test[0], test[1], test[2]) ], shell=True )
+    else:
+	subprocess.call( [ 'fl-run-bench -u %s %s %s.%s' % ( serverURL, test[0], test[1], test[2]) ], shell=True )
 
 # create reports
 for test in tests:
-    cmdLine = 'fl-build-report --html %s' % (test[3])
-
-    subprocess.call([cmdLine])
+    subprocess.call( [ 'fl-build-report --html', test[3] ], shell=True )
 
 print "Testing complete."
