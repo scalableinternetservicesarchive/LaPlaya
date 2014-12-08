f90194a3fa9a8b0040aa832d6ec92cd826b3e3bd
Jasen Hall <jasen@cs.ucsb.edu>
2014-12-07 18:28:28 -0800

    benchTest.py script ready for testing. Looks like a simple-ish change to make it distributed.

--- test-LaPlaya/benchTest.py
+++ test-LaPlaya/benchTest.py
@@ -1,20 +1,34 @@
-#!/usr/bin/env python
 import subprocess
+import sys
+
+serverURL = None
 
 # grab URL from stdin
-
+if len(sys.argv) >= 2:
+    serverURL = sys.argv[1]
 
 # test cases, one array entry per test (may be multiple entries per class/file)
 tests = [
-    { "test_CPCreateUser.py", "CPCreateUser", "test_logged_out_signup_path" },
-    { "test_CPComments.py", "CPComments", "test_readonly_view_comment" },
-    { "test_CPShowProjects.py", "CPShowProjects", "test_readonly_view_projects" },
-    { "test_CPHearts.py", "CPHearts", "test_heart_projects" }
+    { "test_CPCreateUser.py", "CPCreateUser", "test_logged_out_signup_path", "cp-createuser-bench.xml" },
+    { "test_CPComments.py", "CPComments", "test_readonly_view_comment", "cp-comments-bench.xml" },
+    { "test_CPShowProjects.py", "CPShowProjects", "test_readonly_view_projects", "cp-showprojects-bench.xml" },
+    { "test_CPHearts.py", "CPHearts", "test_heart_projects", "cp-hearts-bench.xml" }
     ]
 
 # run tests
 for test in tests:
-    # TODO: add argument to substitute stdin URL for config URL
-    subprocess.call(['fl-run-bench', test[0], '%s.%s' % (test[1], test[2])])
+    cmdLine = ""
+    if serverURL = None:
+        cmdLine = 'fl-run-bench %s %s.%s' % (test[0], test[1], test[2])
+    else:
+        cmdLine = 'fl-run-bench -u %s %s.%s' % (serverURL, test[0], test[1], test[2])
+
+    subprocess.call([cmdLine])
 
 # create reports
+for test in tests:
+    cmdLine = 'fl-build-report --html %s' % (test[3])
+
+    subprocess.call([cmdLine])
+
+print "Testing complete."
